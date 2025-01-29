9f99b15796e900c595d8c794f52ecf3a5212aa38
X <86332915+javapythondjango@users.noreply.github.com>
2025-01-29 18:55:33 +0900

    [level 1] Title: 추억 점수, Time: 0.97 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,15 @@
+def solution(name, yearning, photo):
+    
+    dictionary = dict(zip(name, yearning))
+    scores = []
+    for pt in photo:
+        score = 0
+        for p in pt:
+            if p in dictionary:
+                score += dictionary[p]
+        scores.append(score)
+                
+        
+    
+    
+    return scores
